339c0681c7b522df15eec38e4f05acb2ce2f80c4
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-14 23:07:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,6 @@ class CashRegister
           @total+=price*quantity
           @title=title
           @@items_hash[@title] = quantity
-          binding.pry
         end
 
         def apply_discount
