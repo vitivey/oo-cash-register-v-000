cc3e25694316cdf57b78be600660af24ea3d4f8e
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-14 22:54:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
 
         def initialize(discount=0.0)
           @total=0
-          @discount=discount
+          @discount=discount/100.0
         end
 
         def add_item(title, price, quantity=1)
