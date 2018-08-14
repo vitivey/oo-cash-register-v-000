6426e3f228a4ec5e0a4740c0caf0df73b418258e
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-14 22:52:05 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
         end
 
         def apply_discount
-          @total=@total *@discount
+          @total=@total * (@discount/100)
           "After the discount, the total comes to #{total}"
         end
 
